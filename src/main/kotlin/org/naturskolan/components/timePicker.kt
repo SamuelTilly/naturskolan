36dcf07ddd64642a8fb3742b1562b1e553708487
Samuel Tilly <samuel.tilly@gmail.com>
2024-06-05 00:00:39 +0200

    Update timePicker

--- src/main/kotlin/org/naturskolan/components/timePicker.kt
+++ src/main/kotlin/org/naturskolan/components/timePicker.kt
@@ -3,41 +3,47 @@ package org.naturskolan.components
 import kotlinx.html.*
 import java.util.*
 
+fun FlowContent.timePickerIcon(): Unit =
+    div(classes = "rounded-full hover:bg-neutral-200 p-1") {
+        svg(classes = "w-5 h-5") {
+            attributes["fill"] = "currentColor"
+            attributes["viewBox"] = "0 0 512 512"
+            path {
+                d = "M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+            }
+        }
+    }
+
 inline fun FlowContent.timePicker(label: String = "Välj tid", crossinline block: DIV.() -> Unit = {}): Unit {
     val id = UUID.randomUUID().toString()
+    val hours = (1..24).map { i -> i.toString().padStart(2, '0') }
+    val minutes = (0 until 6).map { i -> (i * 10).toString().padStart(2, '0') }
     field(id = id, label = label) {
         attributes["x-data"] = """{
-            |   open: false,
-            |   hours: [...Array(24)].map((_, i) => ("0" + (i + 1)).slice(-2)),
-            |   minutes: [...Array(6)].map((_, i) => ("0" + (i * 10)).slice(-2))
+            |   close(focusAfter) {
+            |       if ($refs.dropdown.open) {
+            |           $refs.dropdown.close()
+            |       }
+            |       focusAfter && focusAfter.focus()
+            |   },
+            |   toggle() {
+            |       if ($refs.dropdown.open) {
+            |           this.close($refs.input)
+            |       } else {
+            |           $refs.dropdown.show()
+            |       }
+            |   }
             |}""".trimMargin()
         attributes["x-on:keydown.escape"] = "$refs.dropdown.close()"
         attributes["x-on:click.away"] = "$refs.dropdown.close()"
         fieldInput(id = id, type = InputType.text) {
-            placeholder = "08:00"
-            attributes["x-on:click"] = """
-                if (open) {
-                    $refs.dropdown.close()
-                } else {
-                    $refs.dropdown.show()
-                    open = true
-                }
-            """.trimIndent()
-            attributes["popovertarget"] = "mypopover"
+            placeholder = "hh:mm"
+            attributes["x-ref"] = "input"
+            attributes["x-on:click"] = "close()"
         }
-        div(classes = "absolute top-0 right-0 px-3 py-2") {
-            svg(classes = "w-2.5 h-2.5 ms-3") {
-                attributes["aria-hidden"] = "true"
-                attributes["fill"] = "none"
-                attributes["viewBox"] = "0 0 10 6"
-                path {
-                    attributes["stroke"] = "currentColor"
-                    attributes["stroke-linecap"] = "round"
-                    attributes["stroke-linejoin"] = "round"
-                    attributes["stroke-width"] = "2"
-                    d = "m1 1 4 4 4-4"
-                }
-            }
+        div(classes = "absolute top-0 bottom-0 right-0 flex items-center px-3 cursor-pointer text-neutral-400 group-hover:text-neutral-500") {
+            attributes["x-on:click"] = "toggle()"
+            timePickerIcon()
         }
         dialog {
             classes = setOf(
@@ -46,40 +52,51 @@ inline fun FlowContent.timePicker(label: String = "Välj tid", crossinline block
                 "bg-white",
                 "border",
                 "border-neutral-200/70",
-                "max-w-lg",
                 "mt-1",
-                "p-4",
+                "ml-0",
+                "py-4",
                 "rounded-lg",
                 "shadow",
                 "z-10"
             )
             attributes["x-ref"] = "dropdown"
-            attributes["x-on:close"] = "open = false"
-            attributes["x-trap"] = "open"
+            attributes["x-transition.origin.top.left"]
             div(classes = "flex h-24 w-32") {
-                div(classes = "flex-1 overflow-auto group") {
-                    tabIndex = "0"
+                ul(classes = "flex-1 overflow-auto group") {
                     attributes["autofocus"] = ""
-                    template {
-                        attributes["x-for"] = "hour in hours"
-                        div {
+                    tabIndex = "-1"
+                    for ((index, hour) in hours.withIndex()) {
+                        li {
                             attributes["aria-role"] = "option"
+                            attributes["aria-label"] = hour
                             classes = setOf(
                                 "text-center",
                                 "transition-colors",
                                 "duration-200",
                                 "hover:bg-neutral-100",
+                                "cursor-pointer"
                             )
-                            attributes["x-text"] = "hour"
+                            tabIndex = if (index == 0) "0" else "-1"
+                            +hour
                         }
                     }
                 }
-                div(classes = "flex-1 overflow-auto") {
-                    tabIndex = "0"
-                    template {
-                        attributes["x-for"] = "minute in minutes"
-                        div(classes = "text-center") {
-                            attributes["x-text"] = "minute"
+                ul(classes = "flex-1 overflow-auto group") {
+                    attributes["autofocus"] = ""
+                    tabIndex = "-1"
+                    for ((index, minute) in minutes.withIndex()) {
+                        li {
+                            attributes["aria-role"] = "option"
+                            attributes["aria-label"] = minute
+                            classes = setOf(
+                                "text-center",
+                                "transition-colors",
+                                "duration-200",
+                                "hover:bg-neutral-100",
+                                "cursor-pointer"
+                            )
+                            tabIndex = if (index == 0) "0" else "-1"
+                            +minute
                         }
                     }
                 }
